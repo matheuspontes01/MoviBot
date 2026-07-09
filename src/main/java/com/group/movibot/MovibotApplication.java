ba231ecc8b1125a1b17544c38686b5eb0defd402
matheuspontes01 <matheus.spontes0406@gmail.com>
2026-07-09 15:36:16 -0300

    base do spring

---
+++ src/main/java/com/group/movibot/MovibotApplication.java
@@ -0,0 +1,13 @@
+package com.group.movibot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovibotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovibotApplication.class, args);
+	}
+
+}
